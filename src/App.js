feeb8ce337bb3b77b0509ee44b12923f53dd15f4
Josy Dom Alexis <josyanimator@gmail.com>
2024-01-28 14:02:59 +0530

    create state and event

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState("josy")
+  const [newName, setNewName] = useState(name)
+  const handleChange = (e)=>{
+    setNewName(e.target.value)
+    console.log(newName)
+  }
+  const handleNewName = () =>{
+    setName(newName)
+    console.log("working")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name}</h1>
+      <input onChange={(e)=>handleChange(e)} name='newName' type='text' placeholder='name' value={newName}/>
+      <button onClick={()=>handleNewName()}>Change name</button>
     </div>
   );
 }
